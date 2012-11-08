fb8002016926025cf8619db271ad8cb9b15d3454
Steve Tooke <steve@heavi.es>
2012-11-08 11:47:03 +0000

    Get "Find a song" scenario to pass.
      - Introduced a test catalogue
      - Extract create_song factory method
      - Separate commands and queries by returning self from commands
      - Encapsulate results within TestUI

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,6 +1,6 @@
 Given /^the following song exists in the Jukebox's catalogue$/ do |table|
-  @song = Song.new(table.hashes.first)
-  jukebox.add_song(@song)
+  @song = create_song(table.hashes.first)
+  catalogue.add_song @song
 end
 
 When /^I search for "(.*?)"$/ do |search_term|
@@ -8,21 +8,50 @@ When /^I search for "(.*?)"$/ do |search_term|
 end
 
 Then /^I should see the song in the results$/ do
-  ui.results.should include(@song)
+  ui.should be_displaying_result(@song)
 end
 
 module Driver
   def jukebox
-    @jukebox ||= Jukebox.new(ui)
+    @jukebox ||= Jukebox.new(ui, catalogue)
+  end
+
+  def create_song(song_details)
+    Song.new(song_details)
   end
 
   def ui
     @ui ||= TestUI.new
   end
 
+  def catalogue
+    @catalogue ||= TestCatalogue.new
+  end
+
   class TestUI
-    def results
-      []
+
+    def displaying_result?(song)
+      @results.include?(song)
+    end
+
+    def display_search_results(results)
+      @results = results
+      self
+    end
+  end
+
+  class TestCatalogue
+    def add_song(song)
+      songs << song
+      self
+    end
+
+    def songs
+      @songs ||= []
+    end
+
+    def search(term)
+      @songs
     end
   end
 end
@@ -36,14 +65,12 @@ class Song
 end
 
 class Jukebox
-  def initialize(ui)
-
-  end
-
-  def add_song(song)
-
+  def initialize(ui, catalogue)
+    @ui, @catalogue = ui, catalogue
   end
 
   def search(term)
+    @ui.display_search_results @catalogue.search(term)
+    self
   end
 end
