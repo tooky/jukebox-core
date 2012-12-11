488949672286f60c987a267e4357182ebc921f75
Steve Tooke <steve@heavi.es>
2012-12-11 14:15:37 +0000

    Refactor specs to remove duplication

--- spec/wedding_jukebox_spec.rb
+++ spec/wedding_jukebox_spec.rb
@@ -2,23 +2,19 @@ require 'wedding_jukebox'
 
 describe FakeCatalogue do
   let(:catalogue) { FakeCatalogue.new }
+  let(:song) { Song.new(artist: 'David Bowie', title: 'We could be heroes') }
+  before { catalogue.add_song song }
 
   context "searching for songs" do
     it "returns exact matching songs" do
-      song = Song.new('artist' => 'David Bowie', 'title' => 'Heroes')
-      catalogue.add_song song
-      catalogue.search('Heroes').should == [song]
+      catalogue.search('We could be heroes').should == [song]
     end
 
     it "returns partial matching songs" do
-      song = Song.new('artist' => 'David Bowie', 'title' => 'We could be heroes')
-      catalogue.add_song song
       catalogue.search('heroes').should == [song]
     end
 
     it "does not include songs that don't match" do
-      song = Song.new('artist' => 'David Bowie', 'title' => 'Heroes')
-      catalogue.add_song song
       catalogue.search('Villains').should_not include(song)
     end
   end
