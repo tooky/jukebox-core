69d2d112ca09f13d2b0e6b6f479276ea479d0a99
Steve Tooke <steve@heavi.es>
2012-12-11 15:27:41 +0000

    Move SpotifyCatalogue into main app code

--- lib/wedding_jukebox.rb
+++ lib/wedding_jukebox.rb
@@ -20,6 +20,42 @@ class Jukebox
   end
 end
 
+require 'json'
+require 'rest_client'
+class SpotifyCatalogue
+  def search(term)
+    results = JSON.parse(query_api(term))
+    SongBuilder.map(results)
+  end
+
+  def query_api(term)
+    RestClient.get("http://ws.spotify.com/search/1/track.json", params: {q: term})
+  end
+
+  class SongBuilder
+    def self.map(data)
+      data['tracks'].map { |data| new(data).song }
+    end
+
+    def initialize(data)
+      @data = data
+    end
+
+    def title
+      @data["name"]
+    end
+
+    def artist
+      @data["artists"].first["name"]
+    end
+
+    def song
+      Song.new title: title, artist: artist
+    end
+  end
+end
+
+
 class FakeCatalogue
   def search(term)
     @songs.select do |song|
